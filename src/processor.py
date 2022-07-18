707db45e57e1b2daabf932ba78ac005b2f1acde2
Matous <bralor92@email.cz>
2022-07-18 15:33:32 +0200

    src/processor.py: Updated the function.

--- src/processor.py
+++ src/processor.py
@@ -3,39 +3,47 @@ src/processor.py - Task processor.
 """
 import os
 
-from src.parser.parser import get_xml_root
-from src.parser.parser import set_xml_attr
+from src.parser import get_xml_root
 
 from src.tasks.tasknames import get_all_tasks
 from src.tasks.tasknames import get_task_names
 from src.tasks.tasknames import create_task_data
-from src.tasks.tasknames import get_only_task_name
+
+from src.description import replace_descriptions
 
 
 def task_processor():
     """
     Run the main runner of the package.
     """
+    # temporary hardwired arguments
     arg_1 = "-t"
     arg_2 = "../python-uvod-do-programovani"
     arg_3 = "course_python-uvod-do-programovani.xml"
+    arg_4 = "../engeto_tasks/"
 
     # get tree of XML and parse the content
     rel_path = os.path.join(arg_2, arg_3)
-
     tree = get_xml_root(rel_path)
-    params = set_xml_attr(tag="solution", name="sourceDir", value="exercises/")
 
     # get XML tags
-    exercise_tags = get_all_tasks(tree.getroot(), "exercise")
+    exercises = get_all_tasks(tree.getroot(), "exercise")
 
-    # get task paths and task data
-    task_names = get_task_names(tree.getroot(), params)
+    # get task names and task data
+    task_names = get_task_names(
+        tree.getroot(), "solution",
+        "sourceDir", "exercises/"
+    )
     task_data = create_task_data(task_names)
 
-    # get only names
-    only_names = get_only_task_name(task_data)
+    output = replace_descriptions(tree, task_data, exercises)
+
 
     print("Done!")
-    return only_names
+    return output
 
+
+def task_attr_processor():
+    """
+    Run the main function for the overwritting the attributes.
+    """
