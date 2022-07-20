9a560815724a7bed9b24969418f7a249a9b22a82
Matous <bralor92@email.cz>
2022-07-20 16:11:36 +0200

    utils.py: Added a module w/ variables.

---
+++ src/task_manager/utils.py
@@ -0,0 +1,22 @@
+lessons = {
+    "L01": "lesson01",
+    "L02": "lesson02",
+    "L03": "lesson03",
+    "L04": "lesson04",
+    "L05": "lesson05",
+    "L06": "lesson06",
+    "L07": "lesson07",
+    "L08": "lesson08",
+    "L09": "lesson09",
+    "L10": "lesson10",
+    "L11": "lesson11",
+    "L12": "lesson12"
+}
+
+lesson01 = {
+    "slicing_string": "rozdeleni_stringu",
+    "string_operations": "spojovani_stringu",
+    "unit_converter": "prevadec_jednotek",
+    "buying_cars": "nakupujeme_auto",
+    "destinatio_1_cz": "destinatio_1"
+}
