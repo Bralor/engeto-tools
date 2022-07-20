eb2cccbe5c4b4e65403c9ec106d62baf5e172992
Matous <bralor92@email.cz>
2022-07-20 16:11:36 +0200

    utils.py: Added a module w/ variables.
    + Added a new name of the task,
    + added a dictionaries for the third and fourth lesson,
    + added a dictionary with the names from the second lesson,
    + added a new dictionary with the tasks fromthe fifth lesson.

---
+++ src/task_manager/utils.py
@@ -0,0 +1,62 @@
+lessons = {
+    "L01": "lesson01",
+    "L02": "lesson02",
+    "L03": "lesson03",
+    "L04": "lesson04",
+    "L05": "lesson05",
+    "L06": "lesson06",
+    "L07": "lesson07",
+    "L08": "lesson08",
+    "L09": "lesson09",
+    "L10": "lesson10",
+    "L11": "lesson11",
+    "L12": "lesson12"
+}
+
+lesson01 = {
+    "slicing_string": "rozdeleni_stringu",
+    "string_operations": "spojovani_stringu",
+    "unit_converter": "prevadec_jednotek",
+    "buying_cars": "nakupujeme_auto",
+    "destinatio_1_cz": "destinatio_1",
+    "string_length": "delka_stringu"
+}
+
+lesson02 = {
+    'list_operations_2': "list_indexovani",
+    'palindrom': "palindrom",
+    'bmi_calculator': "bmi_kalkulacka",
+    'zjisteni_delky_stringu': "delka_stringu",
+    'convert_time': "prevod_casu",
+    'create_list_add_elements': "list_pridavani_hodnot",
+    'list_operations_1': "list_odstranovani_hodnot",
+    'first_letter': "prvni_pismeno",
+    'destinatio_2_cz': "destinatio_2"
+}
+
+lesson03 = {
+    'metody_slovniku_1': "rozsir_slovnik_slovnikem",
+    'metody_slovniku_2': "nahledy_slovniku",
+    'update': "slovnik_ve_slovniku",
+    'overeni_hesla_slovnik': "overeni_hesla",
+    'set_combinations': "sjednoceni_setu"
+}
+
+lesson04 = {
+    'count_numbers': "pocty_cisel",
+    'string_to_list': "ze_stringu_seznam",
+    'vowels': "samohlasky_a_souhlasky",
+    'odd_even_diff': "suda_vs_licha",
+    'fizzbuzz': "fizz_buzz",
+    'divisor': "delitel",
+    'sort': "skript_na_serazeni",
+    'chessboard': "sachovnice",
+    'counter': "pocitadlo"
+}
+
+lesson05 = {
+    'calculator': "kalkulacka",
+    'break': "prerus_vyber",
+    'nejdelsi_slovo': "ukladej_slova",
+    'shopping_cart': "nakupni_kosik"
+}
