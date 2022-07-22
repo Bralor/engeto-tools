dacf6697930764dfe7a6b0c48e7eae3808eb0bad
Matous <bralor92@email.cz>
2022-07-22 17:01:53 +0200

    parser.py: Added a Python module.

---
+++ src/task_manager/parser.py
@@ -0,0 +1,39 @@
+import xml.etree.ElementTree
+
+
+def get_xml_root(filename: str) -> xml.etree.ElementTree.ElementTree:
+    """
+    Get the root of the XML structure.
+
+    :param filename: a name of the xml file.
+    :type filename: str
+    :return: an object with the content of the xml.
+    :rtype: xml.etree.ElementTree.Element
+
+    :Example:
+    >>> isinstance(
+    ...     get_xml_root("src/tests/foo.xml"),
+    ...     xml.etree.ElementTree.ElementTree
+    ... )
+    True
+    """
+    return xml.etree.ElementTree.parse(filename)
+
+
+def set_xml_attr(**kwargs):
+    """
+    Returns a dictionary object with the given parameters.
+
+    :Example:
+    >>> print(set_xml_attr(name="foo", surname="bar"))
+    {'name': 'foo', 'surname': 'bar'}
+    """
+    return {
+        key: val
+        for key, val in kwargs.items()
+    }
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
