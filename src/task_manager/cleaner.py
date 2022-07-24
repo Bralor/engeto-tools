7a2e6dcd935134a02402d68cc227cf8af6383225
Matous <bralor92@email.cz>
2022-07-24 20:39:28 +0200

    cleaner.py: Updated with new function.
    + added a function for selecting attributes.

--- src/task_manager/cleaner.py
+++ src/task_manager/cleaner.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree
+
 
 def select_names(exercises: list, lesson: dict) -> set:
     """
@@ -17,3 +19,24 @@ def select_names(exercises: list, lesson: dict) -> set:
         new_names.add(lesson.get(name))
 
     return new_names
+
+
+def select_attr(
+        element: xml.etree.ElementTree.Element,
+        child: str,
+        attr_name: str
+    ) -> str:
+    """
+    Return a string from the attribute with a path.
+
+    :param element: an exercise element of XML file.
+    :type element: xml.etree.ElementTree.Element
+    :param child: a name of the child
+    :type child: str
+    :param attr_name:
+    :type attr_name:
+    :return:
+    :rtype:
+    """
+    solution = element.find(child)
+    return solution.attrib.get(attr_name, "nan_path")
