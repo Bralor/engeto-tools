310bfe697e4b75909f3bc90e75986b3caeae48d0
MartinS <martin.sedlak777@gmail.com>
2022-07-30 10:17:30 +0200

    Changed name of tests and var to be more explicit, added few tests

--- src/tests/test_cleaner.py
+++ src/tests/test_cleaner.py
@@ -2,16 +2,44 @@ import xml.etree.ElementTree as te
 import task_manager.cleaner as tc
 
 
-def test_if_select_names_returns_expected_result():
+def test_select_names_returns_expected_result():
     tree = te.parse("./exercise.xml")
     root = tree.getroot()
-    exercise = [exe for exe in root.iter("exercise")]
-    assert tc.select_names(exercise, {"L01": "lesson01"}) == {'unit_converter'}
+    exercises = [exe for exe in root.iter("exercise")]
+    assert tc.select_names(exercises) == {'unit_converter'}
 
 
-def test_if_write_description_returns_expected_data_type():
+def test_select_names_returns_expected_data_type():
     tree = te.parse("./exercise.xml")
     root = tree.getroot()
-    exercise = [exe for exe in root.iter("exercise")]
-    result = tc.select_names(exercise, {"L01": "lesson01"})
+    exercises = [exe for exe in root.iter("exercise")]
+    result = tc.select_names(exercises)
     assert isinstance(result, set)
+
+
+def test_select_attr_returns_expected_result():
+    tree = te.parse("./exercise.xml")
+    root = tree.getroot()
+    exercises = [exe for exe in root.iter("exercise")]
+    assert tc.select_attr(exercises[0], "solution", "sourceDir") == 'exercises/L01/unit_converter/solution'
+
+
+def test_select_attr_returns_expected_data_type():
+    tree = te.parse("./exercise.xml")
+    root = tree.getroot()
+    exercises = [exe for exe in root.iter("exercise")]
+    result = tc.select_attr(exercises[0], "solution", "sourceDir")
+    assert isinstance(result, str)
+
+
+def test_split_name_returns_expected_result():
+    assert tc.split_name("engeto-tools/src/task_manager/attributes") == 'task_manager'
+
+
+def test_split_name_returns_expected_data_type():
+    result = tc.split_name("engeto-tools/src/task_manager/attributes")
+    assert isinstance(result, str)
+
+
+def test_split_name_returns_expected_result():
+    assert tc.split_name("engeto-tools/src/task_manager/attributes") == 'task_manager'
