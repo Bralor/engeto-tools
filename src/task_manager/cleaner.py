84a3bf0873a6d871be22d0b97d30baf11d3ed97b
Matouš Holinka <bralor92@email.cz>
2022-08-05 21:41:04 +0200

    cleaner.py: Updated the module.

--- src/task_manager/cleaner.py
+++ src/task_manager/cleaner.py
@@ -2,6 +2,8 @@ import os
 import shutil
 import xml.etree.ElementTree
 
+import task_manager.utils as tu
+
 from task_manager.description import load_lesson_tasks
 
 
@@ -134,6 +136,7 @@ def move_content(lesson_path: str, engeto_repo: str, package: str) -> None:
     :type engeto_repo: str
     :param package: a relative path to the package.
     :type package: str
+
     """
     lesson = os.path.basename(lesson_path)
 
@@ -150,6 +153,102 @@ def move_content(lesson_path: str, engeto_repo: str, package: str) -> None:
             os.path.join(pack_solution, f"{folder}.py"),
             os.path.join(enge_solution, "main.py")
         )
+
+    # lesson = os.path.basename(lesson_path)  # L04
+    # # package_lesson = os.path.basename(package)
+
+    # # existing_tasks = load_lesson_tasks(tu.lessons.get(lesson, "nan_folder"))
+    # # moved = list()
+
+    # for folder in os.listdir(lesson_path):
+        # if folder not in existing_tasks:
+            # continue
+        # pack_solution = os.path.join(package, folder)
+        # enge_solution = os.path.join(
+            # engeto_repo, "exercises", lesson, folder, "solution"
+        # )
+        # # print(enge_solution)
+        # # print(pack_solution)
+
+        # if not os.path.exists(enge_solution) \
+                # or not os.path.exists(pack_solution):
+            # continue
+        # shutil.copyfile(
+            # os.path.join(pack_solution, f"{folder}.py"),
+            # os.path.join(enge_solution, "main.py")
+        # )
+        # moved.append(load_lesson_tasks(package_lesson).get(folder))
+
+    # # diff = set(moved)
+    # # diff = diff.add("__init__.py")
+    # # for folder in set(os.listdir(package)).difference(diff):
+        # # if os.path.exists(os.path.join(lesson_path, folder)):
+            # # continue
+        # # pack_solution = os.path.join(package, folder)
+        # # enge_solution = os.path.join(
+            # # engeto_repo, "exercises", lesson, load_lesson_tasks(tu.lessons.get(lesson)).get(folder), "solution"
+        # # )
+        # # print(pack_solution)
+        # # print(enge_solution)
+
+        # # shutil.copyfile(
+            # # os.path.join(pack_solution, f"{folder}.py"),
+            # # os.path.join(enge_solution, "main.py")
+        # # )
+
+
+def add_missing_task(engeto_tasks: str, package_tasks: str) -> None:
+    """
+    Create a task folder with content if the task is not part of current
+    repository.
+
+    :Example:
+    >>> package = "../engeto_tasks/tasks/lesson04"
+    >>> repository = "../python-for-cyklus/exercise/L04"
+    >>> add_missing_task(package, repository)
+    """
+    repository = set(os.listdir(engeto_tasks))
+    package = set(os.listdir(package_tasks))
+    diff = package.difference(repository)
+
+    for file in diff:
+        rel_path_repo = os.path.join(package_tasks, file)
+
+        if file != "__init__.py":
+            # print(rel_path_repo)
+            # print(engeto_tasks)
+            # print(package_tasks)
+            create_task_folder(rel_path_repo, engeto_tasks, package_tasks)
+
+
+def create_task_folder(rel_path: str, repository: str, package: str) -> None:
+    """
+    Create a new folder for the missing task. Then create subfolders 'skeleton'
+    'solution' and the file 'skeleton/main.py'.
+
+    :param rel_path: a relative path of the folder.
+    :type rel_path: str
+    :param repository: a relative path of the new folder.
+    :type repository: str
+    :param package: a relative path of the pattern task folder.
+    :type package: str
+    """
+    # os.mkdir(rel_path)
+    print("creating...", repository)
+
+    for subfolder in "skeleton", "solution":
+        print("creating subs..", subfolder)
+        # os.mkdir(os.path.join(rel_path, subfolder))
+
+    # os.mknod("skeleton/main.py")
+    # shutil.copyfile(
+        # os.path.join(package, f"{os.path.basename(rel_path)}.py"),
+        # os.path.join(repository, "solution", "main.py")
+    # )
+    print(
+        os.path.join(package, f"{os.path.basename(rel_path)}.py"),
+        os.path.join(repository, "solution", "main.py")
+    )
 
 
 if __name__ == "__main__":
